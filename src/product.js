a671c4fc3323516c0081f33a864644055daadd54
Zakariye Aden <zakariye.aden00@gmail.com>
2022-03-23 14:49:22 -0500

    starting files

---
+++ src/product.js
@@ -0,0 +1,27 @@
+const PRODUCTS = [
+  {
+    id:0,
+    name:"Sasuke Uchiha",
+    img:"https://www.google.com/search?q=sasuke+uchiha&sxsrf=APq-WBsfzTUtMP9MDQc-3DUxonv0ktXZbw:1648062616584&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjmy9DW99z2AhWQZ80KHbgPBrsQ_AUoAXoECAIQAw&biw=1440&bih=717&dpr=2#imgrc=3enzXR4_Zh5XlM",
+    price:500,
+  },
+  {
+    id:1,
+    name:"Naruto Uzamaki",
+    img:"https://www.google.com/search?q=naruto+uzumaki&tbm=isch&ved=2ahUKEwi62eOg-Nz2AhUJb80KHY_-DTgQ2-cCegQIABAA&oq=Naruto+&gs_lcp=CgNpbWcQARgBMgcIIxDvAxAnMgcIABCxAxBDMgcIABCxAxBDMgcIABCxAxBDMgcIABCxAxBDMgcIABCxAxBDMgQIABBDMgcIABCxAxBDMgQIABBDMgcIABCxAxBDOggIABCABBCxAzoFCAAQgAQ6CggjEO8DEOoCECdQj7YKWJnTCmDT3wpoA3AAeACAAWiIAcINkgEEMjAuMZgBAKABAaoBC2d3cy13aXotaW1nsAEKwAEB&sclient=img&ei=NHE7YrrvBInetQaP_bfAAw&bih=717&biw=1440#imgrc=OBXKSVc_feXZhM",
+    price:500,
+  },
+  {
+    id:2,
+    name:"Itachi Uchiha",
+    img:"https://www.google.com/search?q=itachi+uchiha&tbm=isch&ved=2ahUKEwjR5bD4-Nz2AhVmA50JHZLiA0AQ2-cCegQIABAA&oq=itac&gs_lcp=CgNpbWcQARgAMgcIABCxAxBDMgcIABCxAxBDMgcIABCxAxBDMgcIABCxAxBDMggIABCABBCxAzIICAAQgAQQsQMyBAgAEEMyBwgAELEDEEMyBwgAELEDEEMyBwgAELEDEEM6BwgjEO8DECc6BQgAEIAEOgsIABCABBCxAxCDAToKCCMQ7wMQ6gIQJ1Ds9QhYi40JYP2WCWgDcAB4AYABxAKIAaQNkgEGMTcuMy0xmAEAoAEBqgELZ3dzLXdpei1pbWewAQrAAQE&sclient=img&ei=63E7YpHEMOaG9PwPksWPgAQ&bih=717&biw=1440#imgrc=_tpbS_J32ciPLM",
+    price:1000,
+  },
+  {
+    id:3,
+    name:"Minato Namikaze",
+    img:"https://www.google.com/search?q=minato+namikaze&tbm=isch&ved=2ahUKEwjJ6oTX-dz2AhWRBc0KHR-7Ar8Q2-cCegQIABAA&oq=minat&gs_lcp=CgNpbWcQARgAMgcIABCxAxBDMgcIABCxAxBDMgcIABCxAxBDMgcIABCxAxBDMggIABCABBCxAzIECAAQQzIECAAQQzIICAAQgAQQsQMyBAgAEEMyCAgAEIAEELEDOgUIABCABDoHCCMQ7wMQJzoLCAAQgAQQsQMQgwE6CggjEO8DEOoCECdQ2ZgFWKyxBWDUugVoA3AAeACAAWqIAfQLkgEEMTYuMpgBAKABAaoBC2d3cy13aXotaW1nsAEKwAEB&sclient=img&ei=snI7YsnJEpGLtAaf9or4Cw&bih=717&biw=1440#imgrc=HO4SU5qkyelc8M",
+    price:1000,
+  },
+];
+export default PRODUCTS;
---
+++ src/routes/home.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react'
+import '../style/home.css'
+import { Link } from "react-router-dom";
+function Home() {
+  useEffect(() => {
+    document.title = "Home"
+  });
+  return (
+    <div className="home">
+      <h1>Naruto Shippuden</h1>
+      <Link to="/shop">See out Shop {">"}</Link>
+    </div>
+  );
+}
+
+export default Home
