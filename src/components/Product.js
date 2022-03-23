9cd166dfae402f933145593e2c88e8f51625d35c
Zakariye Aden <zakariye.aden00@gmail.com>
2022-03-23 16:02:35 -0500

    adding product file

---
+++ src/components/Product.js
@@ -0,0 +1,12 @@
+import '../style/Product.css'
+function Product(props) {
+  return (
+    <div className="product">
+      <img src={props.img} alt=""/>
+      <p className="product-name">{props.name}</p>
+      <p className="product-price">{props.price}</p>
+    </div>
+  );
+}
+
+export default Product
