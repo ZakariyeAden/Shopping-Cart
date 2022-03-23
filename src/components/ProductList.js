c23b813735224457407f6b2ba5f86fb3d2725b43
Zakariye Aden <zakariye.aden00@gmail.com>
2022-03-23 16:03:14 -0500

    adding productlist file

---
+++ src/components/ProductList.js
@@ -0,0 +1,21 @@
+import '../style/ProductList.css'
+import Product from './product';
+import { Link } from 'react-router-dom'
+function ProductList(props) {
+  const { products} = props;
+  return (
+    <div className="product-list">
+      {products.map((product, i) => (
+        <Link to={`/product/${product.name}`}>
+          <Product
+            name={product.name}
+            img={product.img}
+            price={product.price}
+          />
+        </Link>
+      ))}
+    </div>
+  );
+}
+
+export default ProductList
