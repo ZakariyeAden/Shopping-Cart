5b7a390429141e182c21142d7d69465f81711d98
Zakariye Aden <zakariye.aden00@gmail.com>
2022-03-24 16:00:32 -0500

    finish shop

---
+++ src/routes/Shop.js
@@ -0,0 +1,17 @@
+import "../style/Shop.css";
+import { useEffect } from "react";
+import ProductList from "../components/ProductList";
+
+function Shop(props) {
+  useEffect(() => {
+    document.title = "Shop";
+  });
+  return (
+    <div className="shop">
+      <h1>Shop</h1>
+      <ProductList products={props.products} />
+    </div>
+  );
+}
+
+export default Shop;
